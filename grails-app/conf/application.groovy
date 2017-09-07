7f6a65dbdc9993185b8e2a82f86a3e8e40f80625
Tom Romano <tom.romano@gmail.com>
2017-09-07 12:59:42 +1000

    altering production postGres dialect to see if that works with heroku now on Postgres 9.6

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -20,7 +20,7 @@ environments {
     dataSource {
       dbCreate = "update"
       driverClassName = "org.postgresql.Driver"
-      dialect = org.hibernate.dialect.PostgreSQLDialect
+      dialect = org.hibernate.dialect.PostgreSQL9Dialect
       uri = new URI(System.env.DATABASE_URL?:"postgres://localhost:5432/test")
       url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require"
       username = uri.userInfo.split(":")[0]
